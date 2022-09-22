59291a13c6f1d5a22f5ab8d6a8c629eba45baf62
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-22 18:20:20 +0200

    created MainComponent

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <HeaderComponent />
-    
+    <MainComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent
-  }
+    HeaderComponent,
+    MainComponent
+}
 }
 </script>
 
@@ -21,5 +24,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1> -- Content Goes Here -- </h1>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'MainComponent'
+}
+</script>
+
+<style scoped lang="scss">
+    main {
+        min-height: 15vh;
+        margin-top: 2rem;
+        padding: 2rem;
+        background-color: #1c1c1c;
+        color: #fff;
+    }
+</style>
