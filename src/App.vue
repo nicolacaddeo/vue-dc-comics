6b8c1e2e921bebe7c60e4e81629be89620827af5
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-09-23 21:24:19 +0200

    added jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <HeaderComponent />
+    <JumboTronComponent />
     <MainComponent />
     <NavComponent />
     <FooterComponent />
@@ -9,24 +10,26 @@
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import JumboTronComponent from './components/JumboTronComponent.vue';
 import MainComponent from './components/MainComponent.vue';
 import NavComponent from './components/NavComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
-
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
+    JumboTronComponent,
     MainComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    
 }
 }
 </script>
 
 <style lang="scss">
-#app {
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -37,7 +40,7 @@ export default {
   margin: 0;
   box-sizing: border-box;
 }
-
 </style>
+
 
 
---
+++ src/components/JumboTronComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="jumbotron">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'JumboTron'
+}
+</script>
+
+<style scoped lang="scss">
+    #jumbotron {
+        height: 40vh;
+        background-image: url('../assets/img/jumbotron.jpg');
+    }
+</style>
+        
